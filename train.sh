f0d54d1deaef393d98df1eb5475c3cd8934452ba
YJ-jenny <yejee.shin.ee@gmail.com>
2024-08-14 17:16:02 +0900

    Update train.sh

--- train.sh
+++ train.sh
@@ -1,5 +1,5 @@
 export OPENAI_LOGDIR="./result"
 
-MODEL_FLAGS="--data_dir /mai_nas/BYS/BlochGAN/Bloch_GAN/data_split --lr 1e-4 --weight_decay 0.05 --save_interval 10000 --batch_size 8 --attention_resolutions 32,16,8 --class_cond False --diffusion_steps 1000 --image_size 128 --learn_sigma False --noise_schedule cosine --num_channels 192 --num_head_channels 64 --num_res_blocks 1 --resblock_updown True --use_fp16 False --use_scale_shift_norm True --predict_xstart True" 
+MODEL_FLAGS="--data_dir data_path --lr 1e-4 --weight_decay 0.05 --save_interval 10000 --batch_size 8 --attention_resolutions 32,16,8 --class_cond False --diffusion_steps 1000 --image_size 128 --learn_sigma False --noise_schedule cosine --num_channels 192 --num_head_channels 64 --num_res_blocks 1 --resblock_updown True --use_fp16 False --use_scale_shift_norm True --predict_xstart True" 
 
 python scripts/mrdiff_train.py $MODEL_FLAGS
